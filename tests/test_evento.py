9ffab4b1fa3e5bf425c4db4a8d8e099113b77c52
Rail Neira <r.neira03@ufromail.cl>
2022-12-28 03:38:13 +0000

    Se agregan tests para validación de eventos y vectores

---
+++ tests/test_evento.py
@@ -0,0 +1,68 @@
+from server.app import validar_evento
+
+def test_no_es_dict():
+    event1 = ''
+    event2 = 2
+    event3 = []
+    assert validar_evento(event1) == False
+    assert validar_evento(event2) == False
+    assert validar_evento(event3) == False
+
+def test_sin_filename():
+    event = {
+        'data': []
+    }
+    assert validar_evento(event) == False
+
+def test_sin_data():
+    event = {
+        'filename': ''
+    }
+    assert validar_evento(event) == False
+
+def test_con_formato_de_vectores_incorrecto():
+    event = {
+        'data': [{'x':0}],
+        'filename': ''
+    }
+    assert validar_evento(event) == False
+
+def test_data_no_es_array():
+    event = {
+        'data': 0,
+        'filename': ''
+    }
+    assert validar_evento(event) == False
+
+def test_filename_no_es_string():
+    event = {
+        'data': [],
+        'filename': 1
+    }
+    assert validar_evento(event) == False
+
+def test_sin_datos_en_data():
+    event = {
+        'data': [],
+        'filename': ''
+    }
+    assert validar_evento(event) == False
+
+def test_con_todo():
+    event = {
+        'filename': 'nombre',
+        'time_start': 'timestamp',
+        'data': [
+            {
+                'time'  : 0.0,
+                'acc_x' : 0.0,
+                'acc_y' : 0.0,
+                'acc_z' : 0.0,
+                'gyro_x': 0.0,
+                'gyro_y': 0.0,
+                'gyro_z': 0.0,
+                'temp'  : 0.0
+            },
+        ] 
+    }
+    assert validar_evento(event) == True
---
+++ tests/test_vector.py
@@ -0,0 +1,24 @@
+from server.app import validar_vector
+
+def test_vector_vacio():
+    assert validar_vector({}) == False
+
+def test_vector_sin_atributos_necesarios():
+    vector = {
+        'hola':1
+    }
+    assert validar_vector(vector) == False
+
+def test_vector_sin_tipos_correctos():
+    vector = {
+        'time'  : 'hola',
+        'acc_x' : 1,
+        'acc_y' : 1,
+        'acc_z' : 1,
+        'gyro_x': 1,
+        'gyro_y': 1,
+        'gyro_z': 1,
+        'temp'  : 1
+    }
+    assert validar_vector(vector) == False
+
