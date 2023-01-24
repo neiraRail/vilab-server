4c85691ddcca0cefb6f521343bc7178d7030ba58
Rail Neira <neira.rail@gmail.com>
2023-01-24 16:19:34 -0300

    Se cambia host de mongodb para docker

--- app.py
+++ app.py
@@ -24,7 +24,7 @@ logging.basicConfig(
 app = Flask(__name__)
 app.config["MONGODB_SETTINGS"] = {
     "db": "vibration_db",
-    "host": "localhost",
+    "host": "172.17.0.1",
     "port": 27017,
 }
 # app.config["MONGO_URI"] = "mongodb://localhost:27017/vibration_db"
