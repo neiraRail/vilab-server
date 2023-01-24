ba41eeeede4e721e0a7bd27d4d0a2ff3b1476b66
Rail Neira <neira.rail@gmail.com>
2023-01-24 15:45:18 -0300

    Se arreglan archivos para que los modulos funcionen tanto en ejecución como en pruebas

--- tests/test_vector_old.py
+++ tests/test_vector_old.py
@@ -1,24 +1,24 @@
-from src.app import validar_vector_old as validar_vector
+from app import validar_vector_old as validar_vector
+
 
 def test_vector_vacio():
     assert validar_vector({}) == False
 
+
 def test_vector_sin_atributos_necesarios():
-    vector = {
-        'hola':1
-    }
+    vector = {"hola": 1}
     assert validar_vector(vector) == False
+
 
 def test_vector_sin_tipos_correctos():
     vector = {
-        'time'  : 'hola',
-        'acc_x' : 1,
-        'acc_y' : 1,
-        'acc_z' : 1,
-        'gyro_x': 1,
-        'gyro_y': 1,
-        'gyro_z': 1,
-        'temp'  : 1
+        "time": "hola",
+        "acc_x": 1,
+        "acc_y": 1,
+        "acc_z": 1,
+        "gyro_x": 1,
+        "gyro_y": 1,
+        "gyro_z": 1,
+        "temp": 1,
     }
     assert validar_vector(vector) == False
-
