c329a6cfd52b7a4461210fa2ada832519ab96179
Rail Neira <neira.rail@gmail.com>
2023-03-27 19:48:46 -0300

    Se actualiza test para cambios en node

--- tests/test_nodo.py
+++ tests/test_nodo.py
@@ -6,10 +6,13 @@ def test_nodo_correcto():
         "ssid": "DRAGINO_IOT",
         "password": "Jota.2020",
         "serverREST": "http://200.13.5.47:8080/eventos",
+        "serverREST2": "",
         "node": 2,
         "time_event": 2500,
         "delay_sensor": 200,
         "time_reset": 24,
+        "batch_size": 1,
         "token": "108160136",
+        "detail": "",
     }
     assert validar_nodo(nodo) == {"valido": True, "razon": None}
